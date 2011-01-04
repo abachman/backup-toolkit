c5ca67f0189ff363a8bbe822be561e3001f60481
Adam Bachman <adam.bachman@gmail.com>
2011-01-04 14:52:07 -0500

    improved install script instructions

--- simple/install.sh
+++ simple/install.sh
@@ -1,4 +1,4 @@
-# Install thyself.
+# Install or update thyself.
 #
 # On the server to be backed up, run:
 #   curl https://github.com/abachman/backup-toolkit/raw/master/simple/install.sh | sh
@@ -7,8 +7,12 @@
 # if you get ssl certificate problems
 
 mkdir -p ~/bin
+
+echo 'getting backup-runner.sh'
 curl -k https://github.com/abachman/backup-toolkit/raw/master/simple/backup-runner.sh > ~/bin/backup-runner.sh
+echo 'getting tar-dump.sh'
 curl -k https://github.com/abachman/backup-toolkit/raw/master/dist/tar-dump.sh > ~/bin/tar-dump.sh
+echo 'getting mysql-dump.sh'
 curl -k https://github.com/abachman/backup-toolkit/raw/master/dist/mysql-dump.sh > ~/bin/mysql-dump.sh
 
 for f in backup-runner.sh tar-dump.sh mysql-dump.sh; do
@@ -19,6 +23,7 @@ for f in backup-runner.sh tar-dump.sh mysql-dump.sh; do
   fi
 done
 
+if [ ! -e ~/MYPROJECT.backup-runner.conf ]; then
 cat <<EOF > ~/MYPROJECT.backup-runner.conf
 # backup-runner configuration values, all must be populated
 
@@ -35,9 +40,20 @@ REMOTE_USER=
 REMOTE_HOST=
 REMOTE_DIR=
 EOF
+fi
 
-printf "The files are in place, now you have to create a cron task. For example:\n\n"
-printf "  # example cronjob\n  30 01 * * * /home/deploy/bin/backup-runner /home/deploy/MYPROJECT.backup-runner.conf\n\n" >&2
-printf "Or, you can add a small script to /etc/cron.daily. For example: \n\n" >&2
-printf "  sudo echo -n '#!/bin/sh\\n/home/deploy/bin/backup-runner.sh /home/deploy/MYPROJECT.backup-runner.conf' > /etc/cron.daily/backup-runner\n" >&2
-printf "  sudo chmod +x /etc/cron.daily/backup-runner\n\n" >&2
+echo
+echo "========================================================================"
+echo "The files are in place, now you have to create a cron task. For example:"
+echo
+echo "  # example cronjob"
+echo "  30 01 * * * /home/deploy/bin/backup-runner.sh /home/deploy/MYPROJECT.backup-runner.conf"
+echo
+echo "Or create a small script like this:"
+echo
+echo "  #!/bin/sh"
+echo "  /home/deploy/bin/backup-runner.sh /home/deploy/MYPROJECT.backup-runner.conf"
+echo
+echo "and add it to /etc/cron.daily."
+echo "========================================================================"
+echo
